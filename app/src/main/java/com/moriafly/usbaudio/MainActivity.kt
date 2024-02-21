a35f25d46c5b4f62b3d571894a2e50bf3cea6762
licoba <licoba@qq.com>
2024-02-21 18:26:52 +0800

    no message

--- app/src/main/java/com/moriafly/usbaudio/MainActivity.kt
+++ app/src/main/java/com/moriafly/usbaudio/MainActivity.kt
@@ -2,17 +2,35 @@ package com.moriafly.usbaudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import au.id.jms.usbaudio.UsbAudio
+import com.hjq.permissions.Permission
+import com.hjq.permissions.XXPermissions
 import com.moriafly.usbaudio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var mBinding: ActivityMainBinding
+    lateinit var mUsbAudio: UsbAudio
 
     override fun onCreate(savedInstanceState: Bundle?) {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
+        initLib()
+        initPermission()
         mBinding.initListener()
+    }
+
+
+    fun initPermission() {
+        val permissions = mutableListOf(Permission.RECORD_AUDIO)
+        // 请求USB读写权限
+        XXPermissions.with(this).permission(permissions).request(null)
+    }
+
+    private fun initLib() {
+//        System.loadLibrary("usbaudio");
+//        mUsbAudio = UsbAudio()
+//        mUsbAudio.loop()
     }
 
     fun ActivityMainBinding.initListener() {
